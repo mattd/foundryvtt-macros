6bf33edd55747853f318b6786315986b9a583435
Matt Dawson <matthewtdawson@gmail.com>
2022-05-03 23:23:27 -0400

    Add select dice count macro

---
+++ select-dice-count.js
@@ -0,0 +1,24 @@
+const content =  `
+    <form>
+        <div class="form-group">
+            <label>Number of Dice:</label>
+            <div class="form-fields">
+                <input name="dice-count" value="0">
+            </div>
+        </div>
+    </form>
+`;
+new Dialog({
+    title: "Roll d[DIE TYPE]", // edit this to your liking.
+    content,
+    buttons: {
+        roll: {
+            label: "Roll!",
+            callback: async (html) => {
+                const count = html.find("[name=dice-count]")[0].value;
+                const roll = await new Roll(`${count}d[DIE TYPE]`).evaluate();
+                await roll.toMessage();
+            }
+        }
+    }
+}).render(true);
